06f2ced12b05606d0a6cdc6d36643178c9e8a49c
Boldyrev Anatoly <boldirev87@mail.ru>
2023-05-18 16:43:58 +0400

    changed test

--- src/test/java/PostmanTest.java
+++ src/test/java/PostmanTest.java
@@ -14,6 +14,6 @@ public class PostmanTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data",equalTo("some data"));
+                .body("data",equalTo("some"));
     }
 }
